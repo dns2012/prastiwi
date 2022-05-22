0fd21b981628480bd4734d05738ec35dfe835dad
dns2012 <dnsprogress@gmail.com>
2022-05-22 08:49:32 +0700

    remove redis from user loan and saving

--- app/Http/Controllers/API/V1/UserController.php
+++ app/Http/Controllers/API/V1/UserController.php
@@ -9,7 +9,6 @@ use App\Models\User;
 use App\Services\Uploader;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redis;
 
 class UserController extends BaseController
 {    
@@ -30,16 +29,7 @@ class UserController extends BaseController
      */
     public function mySaving(): JsonResponse
     {
-        $key = "saving-" . $this->memberId();
-        if (! empty(Redis::get($key))) {
-            $redisValue = json_decode(Redis::get($key), true);
-            return $this->success([
-                'items' => $redisValue, 
-                'total' => array_sum(array_column($redisValue, 'value'))
-            ]);
-        }
         $savings = Saving::where('member_id', $this->memberId())->get()->toArray();
-        Redis::set($key, json_encode($savings));
         return $this->success([
             'items' => $savings, 
             'total' => array_sum(array_column($savings, 'value'))
@@ -53,16 +43,10 @@ class UserController extends BaseController
      */
     public function myLoan(): JsonResponse
     {
-        $key = "loan-" . $this->memberId();
-        if (! empty(Redis::get($key))) {
-            $redisValue = json_decode(Redis::get($key), true);
-            return $this->success($redisValue);
-        }
         $loans = Loan::where('member_id', $this->memberId())->get()->toArray();
         foreach($loans as $index => $loan) {
             $loans[$index]['debt'] = ($loan['loan_value'] + $loan['loan_interest']) - ($loan['loan_value_paid'] + $loan['loan_interest_paid']);
         }
-        Redis::set($key, json_encode($loans));
         return $this->success($loans);
     }
     
